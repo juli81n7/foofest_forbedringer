ae25dbec61dd2f2fa6cfcb8b02eace7cad88edbc
magn5310 <magn5310@stud.kea.dk>
2023-11-29 15:15:41 +0100

    secbtn

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Button from "@/components/Button";
+import SecondaryBtn from "@/components/SecondaryBtn";
 
 export default function Home() {
   return (
     <main>
       <Button></Button>
+      <SecondaryBtn></SecondaryBtn>
     </main>
   );
 }
---
+++ src/components/SecondaryBtn.jsx
@@ -0,0 +1,5 @@
+import "../styles/secondaryBtn.css";
+
+export default function SecondaryBtn() {
+  return <button className="secbtn">View more</button>;
+}
