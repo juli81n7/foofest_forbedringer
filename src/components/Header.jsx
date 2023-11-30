f6cd259588c5cff6758627c17113543265a09b0b
magnus madsen <magn@stud.kea.dk>
2023-11-30 13:33:45 +0100

    nul conflicts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,16 +7,15 @@ function Header() {
   return (
     <header>
       <h1 className="headline">FOOFEST</h1>
-      <section className="headersection">
-        <div className="teasers">
-          <p>Get ready for the party of the year.</p>
-          <p>All your favorite underground artists that you haven´t heard of yet.</p>
-          <p>Grab your crew and get your tickets before they are sold out and prepare yourselves for an experience you won´t soon forget.</p>
-          {/* indsæt <Button/> her i stedet?? */}
-          <Button href="/tickets" btntext="get tickets"></Button>
-        </div>
-        <Image className="hero-img" src="/partypeople.png" width={712} height={451} alt="Picture of a nightime concert, full of festive people" />
-      </section>
+
+      <div className="teasers">
+        <p>Get ready for the party of the year.</p>
+        <p>All your favorite underground artists that you haven´t heard of yet.</p>
+        <p>Grab your crew and get your tickets before they are sold out and prepare yourselves for an experience you won´t soon forget.</p>
+        {/* indsæt <Button/> her i stedet?? */}
+        <Button href="/tickets" btntext="get tickets"></Button>
+      </div>
+      <Image className="hero-img" src="/partypeople.png" width={712} height={451} alt="Picture of a nightime concert, full of festive people" />
     </header>
   );
 }
