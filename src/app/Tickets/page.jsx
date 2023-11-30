298d985a7ccf388a8bcab8a96077b5462e02659c
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-11-30 11:47:07 +0100

    Ændret ting

---
+++ src/app/Tickets/page.jsx
@@ -0,0 +1,23 @@
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import SecondaryBtn from "@/components/SecondaryBtn";
+import TicketCard from "@/components/TicketCard";
+import ArtistCard from "@/components/ArtistCard";
+import ArtistList from "@/components/ArtistList";
+import TicketVisual from "@/components/TicketVisual";
+import "../../styles/Tickets.css";
+
+export default function Tickets() {
+  return (
+    <main>
+<section className="landing">
+<h1>Get your tickets</h1>
+<TicketVisual/>
+<Button btntext="Buy now"/>
+</section>
+
+
+    </main>
+  );
+}
