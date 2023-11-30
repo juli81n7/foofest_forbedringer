2ae6b92371fe7a9689133651d22e71949139e3c3
magnus madsen <magn@stud.kea.dk>
2023-11-30 10:57:25 +0100

    navbar i layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,14 @@
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
 export const metadata = {};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Button from "@/components/Button";
 import Header from "@/components/Header";
-import Navbar from "@/components/Navbar";
+
 import SecondaryBtn from "@/components/SecondaryBtn";
 import TicketCard from "@/components/TicketCard";
 import ArtistCard from "@/components/ArtistCard";
@@ -9,7 +9,6 @@ import ArtistList from "@/components/ArtistList";
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <Header />
       <Button btntext="View all"></Button>
       <SecondaryBtn secbtntext="View more"></SecondaryBtn>
