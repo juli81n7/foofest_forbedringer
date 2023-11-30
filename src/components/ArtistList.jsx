81b3d2918b12199c7e132376fb134eb98d313b96
magnus madsen <magn@stud.kea.dk>
2023-11-30 10:46:18 +0100

    artist list

--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -5,7 +5,7 @@ function ArtistList(props) {
   return (
     <article className="artist-list">
       <div className="stage-details">
-        <h3>{props.stageName} Stage name</h3>
+        <h2 className="artisth2">{props.stageName} Stage name</h2>
         <p>{props.stageName} Stage name is the coolest stage in all of FooFest. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, aut.</p>
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
@@ -13,12 +13,13 @@ function ArtistList(props) {
         {/*  {props.artists.map((artist, index) => (
         <Artist artistName={artist.name} artistTime={artist.time} key={index} />
       ))} */}
-        <Artist artistName="Kunsternavn" artistDay="Fri" artistTime="11:30"></Artist>
-        <Artist artistName="Kunsternavn" artistDay="Fri" artistTime="11:30"></Artist>
-        <Artist artistName="Kunsternavn" artistDay="Fri" artistTime="11:30"></Artist>
-        <Artist artistName="Kunsternavn" artistDay="Fri" artistTime="11:30"></Artist>
-        <Artist artistName="Kunsternavn" artistDay="Fri" artistTime="11:30"></Artist>
-        <Artist artistName="Kunsternavn" artistDay="Fri" artistTime="11:30"></Artist>
+        <Artist artistName="ukendt kunstner" artistDay="Fri" artistTime="11:30"></Artist>
+        <Artist artistName="ukendt kunstner" artistDay="Fri" artistTime="11:30"></Artist>
+        <Artist artistName="ukendt kunstner" artistDay="Fri" artistTime="11:30"></Artist>
+        <Artist artistName="ukendt kunstner" artistDay="Fri" artistTime="11:30"></Artist>
+        <Artist artistName="ukendt kunstner" artistDay="Fri" artistTime="11:30"></Artist>
+        <Artist artistName="ukendt kunstner" artistDay="Fri" artistTime="11:30"></Artist>
+        <Artist artistName="ukendt kunstner" artistDay="Fri" artistTime="11:30"></Artist>
       </div>
     </article>
   );
