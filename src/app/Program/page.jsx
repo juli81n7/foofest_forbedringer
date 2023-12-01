36b217110f4c572616c6373f1a8c00dfc9fb56db
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-12-01 11:27:04 +0100

    Opdateret

---
+++ src/app/Program/page.jsx
@@ -0,0 +1,35 @@
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import SecondaryBtn from "@/components/SecondaryBtn";
+import TicketCard from "@/components/TicketCard";
+import ArtistCard from "@/components/ArtistCard";
+import ArtistList from "@/components/ArtistList";
+import TicketVisual from "@/components/TicketVisual";
+import FilterBtn from "@/components/FilterBtn"
+import Calender from "@/components/Calender"
+
+import Image from "next/image";
+import "../../styles/Program.css";
+
+export default function Program() {
+  return (
+    <main>
+    <h1>Program
+    </h1>
+<div className="filters">
+<FilterBtn btntext="Monday"/>
+<FilterBtn btntext="Tuesday"/>
+<FilterBtn btntext="Wednsday"/>
+<FilterBtn btntext="Thursday"/>
+<FilterBtn btntext="Friday"/>
+<FilterBtn btntext="Saturday"/>
+<FilterBtn btntext="Sunday"/>
+</div>
+
+<Calender/>
+
+    
+    </main>
+  );
+}
