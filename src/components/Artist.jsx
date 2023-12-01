13b95bce9811772f9a25a037c3d9a77edb98b07d
magnus madsen <magn@stud.kea.dk>
2023-12-01 09:54:59 +0100

    artist bjælke klikbar

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,17 +1,18 @@
+import Link from "next/link";
 import "../styles/Artist.css";
 
 import SecondaryBtn from "./SecondaryBtn";
 
 function Artist(props) {
   return (
-    <div className="artist-container">
+    <Link href="/ArtistSingleView/page" className="artist-container">
       <div className="artist">
         <h3>{props.artistName}</h3>
         <h5>{props.artistDay}</h5>
         <h5>{props.artistTime}</h5>
       </div>
       <SecondaryBtn href="/ArtistSingleView/page" secbtntext="View more"></SecondaryBtn>
-    </div>
+    </Link>
   );
 }
 
