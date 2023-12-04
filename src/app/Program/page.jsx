f8f47fcf0d2e69ffb5e397c1bbfaf93fe1734862
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-12-04 13:55:30 +0100

    Push laver calender

--- src/app/Program/page.jsx
+++ src/app/Program/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+
 
 import Button from "@/components/Button";
 import Header from "@/components/Header";
@@ -8,15 +8,22 @@ import TicketCard from "@/components/TicketCard";
 import ArtistCard from "@/components/ArtistCard";
 import ArtistList from "@/components/ArtistList";
 import TicketVisual from "@/components/TicketVisual";
+import Content from "@/components/Content";
 import FilterBtn from "@/components/FilterBtn";
 import Calender from "@/components/Calender";
+import { schedule, spots, events, bands} from "../data"
+
+
 
 import Image from "next/image";
 import "../../styles/Program.css";
 
 export default function Program() {
   return (
-    <main>
+
+    <Content schedule={schedule} spots={spots} events={events}>
+
+<main>
       <h1 className="programh1">Program</h1>
       <div className="filters">
         <FilterBtn btntext="Monday" />
@@ -28,7 +35,11 @@ export default function Program() {
         <FilterBtn btntext="Sunday" />
       </div>
 
-      <Calender />
+      <Calender schedule={schedule} bands={bands} />
     </main>
+
+</Content>
+
+  
   );
 }
--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,28 +1,66 @@
+"use client"
+
+
 import "../styles/Calender.css";
-function Calender() {
-  return (
+import CalenderEvent from "./CalenderEvent";
+function Calender(props) {
+  
+  console.log("DATAAAAA", props.schedule)
+  
+const Jotunheim = props.schedule.Jotunheim;
+const Midgard = props.schedule.Midgard;
+const Vanaheim = props.schedule.Vanaheim;
+
+
+    return (
     <section className="calendar-container">
       <div className="scenes">
         <div className="scene">
-          <h4>SVARTHEIM</h4>
+          <h4>JOTUNHEIM</h4>
         </div>
         <div className="scene">
-          <h4>NILFHEIM</h4>
+          <h4>MIDGARD</h4>
         </div>
         <div className="scene">
-          <h4>MUSPELHEIM</h4>
+          <h4>VANAHEIM</h4>
         </div>
-        <div className="scene">
-          <h4>ALFHEIM</h4>
-        </div>
+        
       </div>
       <div className="calender">
         <div className="calenderarea">
+        <div className="calenderbar">
+   <div className="calender_time"><p>1:00</p></div>
+   <div className="calender_time"><p>2:00</p></div>
+   <div className="calender_time"><p>3:00</p></div>
+   <div className="calender_time"><p>4:00</p></div>
+   <div className="calender_time"><p>5:00</p></div>
+   <div className="calender_time"><p>6:00</p></div>
+   <div className="calender_time"><p>7:00</p></div>
+   <div className="calender_time"><p>8:00</p></div>
+   <div className="calender_time"><p>9:00</p></div>
+   <div className="calender_time"><p>10:00</p></div>
+   <div className="calender_time"><p>11:00</p></div>
+   <div className="calender_time"><p>12:00</p></div>
+   <div className="calender_time"><p>13:00</p></div>
+   <div className="calender_time"><p>14:00</p></div>
+   <div className="calender_time"><p>15:00</p></div>
+   <div className="calender_time"><p>16:00</p></div>
+   <div className="calender_time"><p>17:00</p></div>
+   <div className="calender_time"><p>18:00</p></div>
+   <div className="calender_time"><p>19:00</p></div>
+   <div className="calender_time"><p>20:00</p></div>
+   <div className="calender_time"><p>21:00</p></div>
+   <div className="calender_time"><p>22:00</p></div>
+   <div className="calender_time"><p>23:00</p></div>
+   <div className="calender_time"><p>24:00</p></div>
+
+
+   
+       
+    
+      </div>
           <div className="calenderbar">
-            <div className="event">
-              <div className="img_container"></div>
-              <h4>Ukendt kunstner</h4>
-            </div>
+
             <div className="calenderhour"></div>
             <div className="calenderhour"></div>
             <div className="calenderhour"></div>
@@ -47,6 +85,22 @@ function Calender() {
             <div className="calenderhour"></div>
             <div className="calenderhour"></div>
             <div className="calenderhour"></div>
+            {
+                Jotunheim.mon.map((thing) => (
+                    thing.act !== "break" ? (
+                      <CalenderEvent
+
+                        img2={props.bands.find(band => band.name === thing.act).logo}
+                        name={thing.act}
+                        start={thing.start}
+                        end={thing.end}
+
+                      />
+                    ) : null
+                  ))
+                  
+            
+            }
           </div>
           <div className="calenderbar">
             <div className="calenderhour"></div>
@@ -73,6 +127,22 @@ function Calender() {
             <div className="calenderhour"></div>
             <div className="calenderhour"></div>
             <div className="calenderhour"></div>
+            {
+                Midgard.mon.map((thing) => (
+                    thing.act !== "break" ? (
+                      <CalenderEvent
+
+                        img2={props.bands.find(band => band.name === thing.act).logo}
+                        name={thing.act}
+                        start={thing.start}
+                        end={thing.end}
+
+                      />
+                    ) : null
+                  ))
+                  
+            
+            }
           </div>
           <div className="calenderbar">
             <div className="calenderhour"></div>
@@ -99,33 +169,24 @@ function Calender() {
             <div className="calenderhour"></div>
             <div className="calenderhour"></div>
             <div className="calenderhour"></div>
+            {
+                Vanaheim.mon.map((thing) => (
+                    thing.act !== "break" ? (
+                      <CalenderEvent
+
+                        img2={props.bands.find(band => band.name === thing.act).logo}
+                        name={thing.act}
+                        start={thing.start}
+                        end={thing.end}
+
+                      />
+                    ) : null
+                  ))
+                  
+            
+            }
           </div>
-          <div className="calenderbar">
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-            <div className="calenderhour"></div>
-          </div>
+         
         </div>
       </div>
     </section>
