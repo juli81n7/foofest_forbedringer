0b4b279816c99ddc0cb3d26f35460c66d0a63d7b
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-12-05 10:38:37 +0100

    .

--- src/components/ChooseCamp.jsx
+++ src/components/ChooseCamp.jsx
@@ -1,13 +1,14 @@
 import "../styles/ChooseCamp.css";
+import CampArea from "./CampArea";
 function ChooseCamp() {
   return (
     <div>
       <h2>CHOOSE YOUR AREA</h2>
       <div className="camp-box">
-        <div className="camp-area">SVARTHEIM</div>
-        <div className="camp-area">NILFHEIM</div>
-        <div className="camp-area">MUSPELHEIM</div>
-        <div className="camp-area">ALFHEIM</div>
+        <CampArea areaName="SVARTHEIM" />
+        <CampArea areaName="NILFHEIM" />
+        <CampArea areaName="MUSPELHEIM" />
+        <CampArea areaName="ALFHEIM" />
       </div>
     </div>
   );
