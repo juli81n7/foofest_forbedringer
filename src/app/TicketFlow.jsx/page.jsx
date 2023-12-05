d66a3730a0bc234956cb592c4eaff208955a2728
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-12-05 13:44:26 +0100

    .

--- src/app/TicketFlow.jsx/page.jsx
+++ src/app/TicketFlow.jsx/page.jsx
@@ -7,12 +7,15 @@ import "@/styles/TicketFlow.css";
 import ParticipantInfo from "@/components/ParticipantInfo";
 import ChooseCamp from "@/components/ChooseCamp";
 import CheckoutOptions from "@/components/CheckoutOptions";
+import NoTiltTicket from "@/components/NoTiltTicket";
 
 function page() {
   return (
     <main className="main-flow">
       <section className="sec">
-        <div className="ticket-area"></div>
+        <div className="ticket-area">
+          <NoTiltTicket />
+        </div>
         <div className="flow-area">
           <ChooseTicket />
           <ChooseTent />
