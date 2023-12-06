864d47b04348644dbe4d5365a473edc33e7e87ae
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-12-06 17:47:25 +0100

    opdateret en masse

--- src/components/CalenderEvent.jsx
+++ src/components/CalenderEvent.jsx
@@ -7,6 +7,21 @@ function CalenderEvent(props) {
 const start = props.start.slice(0, 2);
 const end = props.end.slice(0, 2);
 
+let myImage=props.img2
+
+if(myImage.startsWith("https")){
+  myImage = myImage
+}
+else{
+  myImage = "http://localhost:8080/logos/"+ myImage
+  console.log(myImage)
+}
+
+
+
+
+
+
 
   return (
 
@@ -14,7 +29,7 @@ const end = props.end.slice(0, 2);
 
     <Link href={`ArtistSingleView/${props.slug}`} className={`event start${ start} end${ end}`}>
     <div className="img_container">
-              <img src={props.img2} alt="" /></div>
+              <img src={myImage} alt="" /></div>
               <div className="text">
               <p>{props.start + "-" + props.end}</p>
               <h4>{props.name}</h4>
