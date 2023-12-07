94aa14d19ff9f0a439d602802c623aa80c16d90e
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-12-07 18:03:33 +0100

    opdateret så både genre og dag virker

--- src/app/ArtistSingleView/[slug]/page.jsx
+++ src/app/ArtistSingleView/[slug]/page.jsx
@@ -8,7 +8,11 @@ import SecondaryBtn from "@/components/SecondaryBtn";
 import RecommendedList from "@/components/RecommendedList";
 import RecommendedArtist from "@/components/RecommendedArtist";
 import { schedule, bands, spots, events } from "../../data";
+
+
+
 import ImageContainer from "@/components/ImageContainer";
+import Link from "next/link";
 
 export async function generateStaticParams() {
   const paths = bands.map((band) => {
@@ -26,7 +30,7 @@ export default function page({ params }) {
 
   const foundObjects = [];
 const genreBands=  bands.filter((band) => band.genre ===selectedBand.genre)
-console.log(selectedBand)
+
 
 
 
@@ -34,45 +38,130 @@ console.log(selectedBand)
 Object.keys(schedule).map((stage) => {
   Object.keys(schedule[stage]).map((day) => {
     const bandFromSchedule = schedule[stage][day].find((act)=> act.act === selectedBand.name)
+
    const scene = stage
    const Plaingday = day
 if(
       bandFromSchedule
-  
-     
-  
+
      ){
      
       foundObjects.band=bandFromSchedule;
       foundObjects.day=day
       foundObjects.stage=stage
-    }
+      if(
+  foundObjects.day === "thu"
+)
+  {
+    console.log(foundObjects.day)
+  }
+
   
+    }
+ })
+ 
+})
 
 
+const foundObjectsForGenre =[]
+
+Object.keys(schedule).map((stage) => {
+Object.keys(schedule[stage]).map((day) => {
     
+  genreBands.map((band)=>{
+    const genreBandsFromSchedules = schedule[stage][day].find((act)=> act.act === band.name)
+const Plaingday = day
+
+if(
+  genreBandsFromSchedules && genreBandsFromSchedules.act !== selectedBand.name
+
+     ){
+const genreBandsFromSchedulesObject = {
+  "bandSchedule":genreBandsFromSchedules,
+  "band": band,
+  "day":Plaingday
+}
+
+      
+     foundObjectsForGenre.push(genreBandsFromSchedulesObject)
+    }
+ })
+})
+
+
+ 
+})
+
+
+const sameDayBands =[]
+
+
+
+
+Object.keys(schedule).map((stage) => {
+  Object.keys(schedule[stage]).map((thisday) => {
+if(thisday===foundObjects.day){
+  schedule[stage][thisday].map((act)=>{
+    if(act.act !== "break" && act.act !== selectedBand.name){
+let myobj = {
+"schedule": act,
+"band":bands.filter((band) => band.name === act.act)[0]
+}
+
+
+
+
+
+sameDayBands.push(myobj)
+
+}
+
+ 
+  })
+}
+
 
 
 
   })
+  
+  
+   
+  })
 
+
+  foundObjectsForGenre.map((obj)=>{
+
+
+if(obj.band.logo.startsWith("https")){
+  obj.band.logo = obj.band.logo
+}
+else{
+  obj.band.logo = "http://localhost:8080/logos/"+ obj.band.logo
+  console.log(obj.band.logo)
+}
+
+
+  })
+
+
+
+
+  sameDayBands.map((obj)=>{
+
+
+    if(obj.band.logo.startsWith("https")){
+      obj.band.logo = obj.band.logo
+
+    }
+    else{
+      obj.band.logo = "http://localhost:8080/logos/"+ obj.band.logo
+      console.log(obj.band.logo)
+    }
     
-
-}          )
-
-
- 
-
-
-
-
-  
-
-
-
-
-  
-
+    
+      })
+    
 
   
   
@@ -106,33 +195,58 @@ if(
       <RecommendedList heading={"View more " + selectedBand.genre}>
 
 
-{
-genreBands.map((band) => (
-    <RecommendedArtist artistName={band.name} img={band.logo} artistTime="19.30"></RecommendedArtist>
-    ))
+
+      {
+        foundObjectsForGenre.map((myband) => (
+<Link href={myband.band.slug}>
+              <div className="recommended-artist-container" key={myband.band.slug}            >
+              <img src={myband.band.logo} alt="Picture of the very well known hiphop artist group, unknown artist"></img>
+              <div className="recommended-artist">
+                <h3>{myband.band.name}</h3>
+                <p>{myband.bandSchedule.start}</p>
+              </div>
+            </div>
+</Link>
+
+            
+             
+            
+          
+          ))
+          
     
+    }
+   
+      
 
-}
 
 
       </RecommendedList>
-      <RecommendedList heading="What else plays friday?">
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
-        <RecommendedArtist artistName="Ukendt Kunstner" artistTime="19:30"></RecommendedArtist>
+      <RecommendedList heading={"What else plays friday on " + foundObjects.stage} >
+        
+      {
+        sameDayBands.map((myband) => (
+<Link href={myband.band.slug}>
+              <div className="recommended-artist-container" key={myband.band.slug}            >
+              <img src={myband.band.logo} alt="Picture of the very well known hiphop artist group, unknown artist"></img>
+              <div className="recommended-artist">
+                <h3>{myband.band.name}</h3>
+                <p>{myband.schedule.start}</p>
+              </div>
+            </div>
+
+</Link>
+            
+             
+            
+          
+          ))
+          
+    
+    }
+
       </RecommendedList>
+      
     </div>
   );
 }
--- src/components/RecommendedArtist.jsx
+++ src/components/RecommendedArtist.jsx
@@ -1,15 +1,46 @@
 import "../styles/RecommendedArtist.css";
 import Image from "next/image";
-
+// schedule={schedule} bands={bands} artistName={band.name} img={band.logo}
 function RecommendedArtist(props) {
+
+
+// let band = ""
+// let logo = ""
+
+
+//       props.band !== undefined ? (
+        
+//         logo = props.band.logo 
+        
+//       ) : null
+
+//       console.log(logo)
+
+
+//       if(logo.startsWith("https")){
+//         logo = logo
+//       }
+//       else{
+//         logo = "http://localhost:8080/logos/"+ logo
+//         console.log(logo)
+//       }
+      
+      
+
+
+
+
+
+
   return (
     <div className="recommended-artist-container">
-      <img src="/ukendt.jpg" alt="Picture of the very well known hiphop artist group, unknown artist"></img>
+      <img src={"/ukendt.jpg"} alt="Picture of the very well known hiphop artist group, unknown artist"></img>
       <div className="recommended-artist">
-        <h3>{props.artistName}</h3>
-        <p>{props.artistTime}</p>
+        <h3>Hej</h3>
+        <p>nu</p>
       </div>
     </div>
+
   );
 }
 
