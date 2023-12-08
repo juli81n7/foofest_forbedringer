ba5c897a94868c0dff7a64e5873352225b1c07f5
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-12-08 13:46:49 +0100

    initial + class=>className

--- src/app/data.js
+++ src/app/data.js
@@ -1,47 +1,76 @@
 // glitch server end-point: "https://flash-metal-egg.glitch.me/ENDPOINT"
-/* const res = await fetch("http://localhost:8080/schedule", {
+
+const res = await fetch(process.env.NEXT_PUBLIC_HOST + "/schedule", {
   headers: {},
 });
 export const schedule = await res.json();
 
-const res2 = await fetch("http://localhost:8080/bands", {
+const res2 = await fetch(process.env.NEXT_PUBLIC_HOST + "/bands", {
   headers: {},
 });
 export const bands = await res2.json();
 
-const res3 = await fetch("http://localhost:8080/available-spots", {
+const res3 = await fetch(process.env.NEXT_PUBLIC_HOST + "/available-spots", {
   headers: {},
 });
 export const spots = await res3.json();
 
-const res4 = await fetch("http://localhost:8080/events", {
-  headers: {},
-});
-export const events = await res4.json(); */
-
-const res = await fetch(process.env.HOST + "/schedule", {
-  headers: {},
-});
-export const schedule = await res.json();
-
-const res2 = await fetch(process.env.HOST + "/bands", {
-  headers: {},
-});
-export const bands = await res2.json();
-
-const res3 = await fetch(process.env.HOST + "/available-spots", {
-  headers: {},
-});
-export const spots = await res3.json();
-
-const res4 = await fetch(process.env.HOST + "/events", {
+const res4 = await fetch(process.env.NEXT_PUBLIC_HOST + "/events", {
   headers: {},
 });
 export const events = await res4.json();
 
+// Tentbooking
+async function putReq(chosenArea, chosenAmount) {
+  let headersList = {
+    "Content-Type": "application/json",
+  };
+  let bodyContent = JSON.stringify({
+    area: chosenArea,
+    amount: chosenAmount,
+  });
+
+  let response = await fetch(process.env.NEXT_PUBLIC_HOST + "/reserve-spot", {
+    method: "PUT",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  console.log(data);
+}
+export function reserveSpot(area, spots) {
+  putReq(area, spots);
+}
+//   reserveSpot("Muspelheim", 3);
+
+// fulfill reservation
+async function postReq(id) {
+  let headersList = {
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    id: id,
+  });
+
+  let response = await fetch(process.env.NEXT_PUBLIC_HOST + "/fullfill-reservation", {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  console.log(data);
+}
+export function fulfillReservation(id) {
+  postReq(id);
+}
+//   fulfillReservation("18h4h4a5slpmiyl9g");
+
+// loginstuff
 let headersList = {
   Accept: "*/*",
-  "User-Agent": "Thunder Client (https://www.thunderclient.com)",
   apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4Y3FzdWtyc2xmbnJ5d3Zra21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5NDE1MzYsImV4cCI6MTk5NzUxNzUzNn0.q1lX-ubiMOiGU0SMT99lf7QauZ0wgy7dyaNSLxTobUg",
 };
 
