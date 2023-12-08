1b3d07547e83efbb2e686bb1934278ed8cb92ed9
Valentin <Chris_valentin@hotmail.com>
2023-12-08 16:20:17 +0100

    profile data

--- src/app/data.js
+++ src/app/data.js
@@ -41,7 +41,6 @@ export const events = await res4.json();
 
 let headersList = {
   Accept: "*/*",
-  "User-Agent": "Thunder Client (https://www.thunderclient.com)",
   apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4Y3FzdWtyc2xmbnJ5d3Zra21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5NDE1MzYsImV4cCI6MTk5NzUxNzUzNn0.q1lX-ubiMOiGU0SMT99lf7QauZ0wgy7dyaNSLxTobUg",
 };
 
@@ -51,3 +50,30 @@ let response = await fetch("https://cxcqsukrslfnrywvkkml.supabase.co/rest/v1/log
 });
 
 export const loginInfo = await response.json();
+
+export async function PostLogin(props) {
+  let headersList = {
+    Accept: "*/*",
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4Y3FzdWtyc2xmbnJ5d3Zra21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5NDE1MzYsImV4cCI6MTk5NzUxNzUzNn0.q1lX-ubiMOiGU0SMT99lf7QauZ0wgy7dyaNSLxTobUg",
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    firstName: "Kong",
+    lastName: "Christian",
+    email: "kongen@hotmail.com",
+    password: "youshallnotpass",
+    phone: "60526220",
+    address: "kongevej 34",
+    zip: "2720",
+  });
+
+  let response = await fetch("https://cxcqsukrslfnrywvkkml.supabase.co/rest/v1/login_info", {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  console.log(data);
+}
