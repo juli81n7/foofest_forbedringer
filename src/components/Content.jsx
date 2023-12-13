a1fb95f804868482ac05c514f10cfce5e6ed2795
magnus madsen <magn@stud.kea.dk>
2023-12-13 15:33:56 +0100

    kurv stuff

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,10 @@
-"use client"
+"use client";
 import { useContext } from "react";
 import { ThemeContext } from "./MyContext";
 
 function Content({ children }) {
-  const state = useContext(ThemeContext)
-
-  return <main data-theme={state?"dark":"light"} >{children}</main>;
+  const state = useContext(ThemeContext);
+  return <main data-theme={state ? "dark" : "light"}>{children}</main>;
 }
 
 export default Content;
