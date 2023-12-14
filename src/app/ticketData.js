e59ad157a389d8e2ed0ac2a637d98a90179f30d1
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-12-14 11:52:01 +0100

    reserving

---
+++ src/app/ticketData.js
@@ -0,0 +1,44 @@
+// Tentbooking
+export async function ReserveSpot(chosenArea, chosenAmount) {
+  let headersList = {
+    "Content-Type": "application/json",
+  };
+  let bodyContent = JSON.stringify({
+    area: chosenArea,
+    amount: chosenAmount,
+  });
+
+  let response = await fetch(process.env.NEXT_PUBLIC_HOST + "/reserve-spot", {
+    method: "PUT",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  //   console.log(data);
+  return data;
+}
+
+//   reserveSpot("Muspelheim", 3);
+
+// fulfill reservation
+export async function fulfillReservation(id) {
+  let headersList = {
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    id: id,
+  });
+
+  let response = await fetch(process.env.NEXT_PUBLIC_HOST + "/fullfill-reservation", {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  console.log(data);
+}
+
+//   fulfillReservation("18h4h4a5slpmiyl9g");
--- src/components/CheckoutOptions.jsx
+++ src/components/CheckoutOptions.jsx
@@ -1,10 +1,15 @@
 "use client";
-import Button from "./Button";
-import { useContext } from "react";
-import "../styles/CheckoutOptions.css";
+import { useContext, useState } from "react";
 import { StateContext, ValueContext } from "./MyContext";
+import { ReserveSpot } from "@/app/ticketData";
+import "../styles/CheckoutOptions.css";
 
 function CheckoutOptions({ setToggleCheckout }) {
+  const state = useContext(ValueContext);
+  const dispatch = useContext(StateContext);
+  const totalTentSpots = state.tents.one + state.tents.two * 2 + state.tents.three;
+  const [resID, setResID] = useState("");
+
   function handleAddToBasket() {
     dispatch((old) => ({
       ...old,
@@ -12,13 +17,18 @@ function CheckoutOptions({ setToggleCheckout }) {
     }));
   }
 
-  const state = useContext(ValueContext);
-  const dispatch = useContext(StateContext);
+  async function reserve() {
+    const a = await ReserveSpot(state.campingArea, totalTentSpots);
+    console.log(a);
+    setResID(a.id);
+    console.log("halløj", resID);
+  }
+
   return (
     <div className="flow-btns">
       <button onClick={handleAddToBasket}>ADD TO BASKET</button>
 
-      <button>BUY NOW</button>
+      <button onClick={reserve}>BUY NOW</button>
     </div>
   );
 }
