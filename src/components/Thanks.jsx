d5b962f38061656d5a1473d77ba728c26fdf7e96
Valentin <Chris_valentin@hotmail.com>
2023-12-14 18:24:01 +0100

    mobile done

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -4,8 +4,10 @@ export default function Thanks() {
   return (
     <div className="thankscontainer">
       <h1>Thank you for your reservation</h1>
-      <h2>Your ordernumber:</h2>
-      <h2>1234</h2>
+      <div>
+        <h2>Your order number:</h2>
+        <h2>1234</h2>
+      </div>
       <Button href={"/Account"} btntext={"View tickets"}></Button>
     </div>
   );
