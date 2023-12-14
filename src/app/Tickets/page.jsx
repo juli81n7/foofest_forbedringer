275d8ba2f13cd3ee4b367b990ea7763ecf3db091
Valentin <Chris_valentin@hotmail.com>
2023-12-14 15:25:38 +0100

    ticket css

--- src/app/Tickets/page.jsx
+++ src/app/Tickets/page.jsx
@@ -20,41 +20,6 @@ export default function Tickets() {
           <Button href="/TicketFlow" btntext="Buy now" />
         </div>
       </section>
-
-      <section className="areaSection">
-        <h2>Camping areas</h2>
-        <p>dksjflsdkjflkdsjflksdjflkjsdlkdsfksdf</p>
-        <div className="areas">
-          <div className="areacard">
-            <div className="img_container">
-              <img src="https://source.unsplash.com/random" alt="" />
-            </div>
-
-            <h4>Alfheim</h4>
-          </div>
-          <div className="areacard">
-            <div className="img_container">
-              <img src="https://source.unsplash.com/random" alt="" />
-            </div>
-
-            <h4>Alfheim</h4>
-          </div>
-          <div className="areacard">
-            <div className="img_container">
-              <img src="https://source.unsplash.com/random" alt="" />
-            </div>
-
-            <h4>Alfheim</h4>
-          </div>
-          <div className="areacard">
-            <div className="img_container">
-              <img src="https://source.unsplash.com/random" alt="" />
-            </div>
-
-            <h4>Alfheim</h4>
-          </div>
-        </div>
-      </section>
     </main>
   );
 }
