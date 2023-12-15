5dd4dea519c3731d97e4a54f8f79bd27e6565951
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-12-15 14:18:44 +0100

    .

--- src/app/Tickets/page.jsx
+++ src/app/Tickets/page.jsx
@@ -2,13 +2,17 @@ import Button from "@/components/Button";
 import Prices from "@/components/Prices";
 import TicketVisual from "@/components/TicketVisual";
 import "../../styles/Tickets.css";
+import Link from "next/link";
 
 export default function Tickets() {
   return (
     <main>
       <section className="landing">
         <h1 className="ticketh1">Get your tickets</h1>
-        <TicketVisual />
+        <Link href="/TicketFlow">
+          <TicketVisual />
+        </Link>
+
         <div className="btn_container">
           <Button href="/TicketFlow" btntext="Buy now" />
         </div>
