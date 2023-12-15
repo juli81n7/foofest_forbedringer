12fe3bf970513ae6bba52fe90a0b29a43341df79
magnus madsen <magn@stud.kea.dk>
2023-12-15 13:33:59 +0100

    green camping n' shit

--- src/components/TicketFormInfo.jsx
+++ src/components/TicketFormInfo.jsx
@@ -3,6 +3,9 @@ import { useContext } from "react";
 import { ValueContext } from "./MyContext";
 export default function TicketFormInfo({ ticketAmount, onePers, twoPers, threePers, selectedArea }) {
   const state = useContext(ValueContext);
+  const totalTents = state.tents.one + state.tents.two + state.tents.three;
+  const totalTentsCrewPrice = state.tents.one * 199 + state.tents.two * 299 + state.tents.three * 399;
+  const totalGreenCamping = totalTents * 249;
   return (
     <div>
       <div className="ticketinfo">
@@ -19,7 +22,7 @@ export default function TicketFormInfo({ ticketAmount, onePers, twoPers, threePe
         </div>
         <div>
           <div className="ticketamount">
-            <p>{state.regular}</p>
+            <p>{state.vip}</p>
             <p>VIP tickets</p>
           </div>
           <div className="ticketpay">
@@ -48,13 +51,25 @@ export default function TicketFormInfo({ ticketAmount, onePers, twoPers, threePe
           </div>
           <p>included</p>
         </div>
+        {state.crewSetup ? (
+          <div className="totalprice">
+            <p>Crew tent setup X {totalTents}</p>
+            <p>{totalTentsCrewPrice} DKK</p>
+          </div>
+        ) : null}
+        {state.greenCamping ? (
+          <div className="totalprice">
+            <p>Green Camping fee X {totalTents}</p>
+            <p>{totalGreenCamping} DKK</p>
+          </div>
+        ) : null}
         <div className="camparea">
           <p>Camping area</p>
           <h3>{state.campingArea}</h3>
         </div>
         <div className="totalprice">
           <p>Total price</p>
-          <p>{state.regular * 799 + state.vip * 1299} DKK</p>
+          <p>{state.regular * 799 + state.vip * 1299 + (state.crewSetup ? totalTentsCrewPrice : null) + (state.greenCamping ? totalGreenCamping : null)} DKK</p>
         </div>
       </div>
     </div>
