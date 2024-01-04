48f2244095d68af9bca2b977d0fa5cbb1a20ab5d
Ballademager <118718923+Ballademager@users.noreply.github.com>
2024-01-04 14:53:30 +0100

    access

--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -14,11 +14,11 @@ function ArtistList(props) {
     <article className="artist-list">
       <div className="stage-details">
         <div>
-          <h3>Todays program</h3>
+          <p>Todays program</p>
           <h2 className={"artisth2 " + props.stageName}>{props.stageName}</h2>
         </div>
         <p>
-          {props.stageName} is one of the coolest stages in all of FooFest. It's in the top 3 of our most visited stages and features some of the festivals most talented artists. Be sure to visit {props.stageName} during your stay!
+          {props.stageName} is one of the coolest stages in all of FooFest. It is in the top 3 of our most visited stages and features some of the festivals most talented artists. Be sure to visit {props.stageName} during your stay!
         </p>
       </div>
       <div className="artist-details">
