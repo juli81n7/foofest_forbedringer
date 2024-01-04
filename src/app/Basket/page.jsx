63cd12602c58df2678357d122b42d3a9b8188cda
Ballademager <118718923+Ballademager@users.noreply.github.com>
2024-01-04 12:31:46 +0100

    metadata

--- src/app/Basket/page.jsx
+++ src/app/Basket/page.jsx
@@ -1,6 +1,12 @@
-"use client";
-
 import BasketContent from "@/components/BasketContent";
+
+export const metadata = {
+  title: "Basket",
+  description: "Foofest is a 7 day festival with around the clock amazing music and events, delicious food and concious camping options.",
+  type: "website",
+  image: process.env.NEXT_PUBLIC_HOST + "/_next/image?url=%2Fpartypeople.png&w=1920&q=75",
+  url: process.env.NEXT_PUBLIC_HOST + "/Basket",
+};
 
 export default function page() {
   return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,15 @@ import Content from "@/components/Content";
 import Header from "@/components/Header";
 import ArtistList from "@/components/ArtistList";
 
+export const metadata = {
+  title: "FooFest",
+  description: "Foofest is a 7 day festival with around the clock amazing music and events, delicious food and concious camping options.",
+  type: "website",
+  // keywords: ["Foofest", "Music Festival", "Seven day festival"],
+  image: process.env.NEXT_PUBLIC_HOST + "/_next/image?url=%2Fpartypeople.png&w=1920&q=75",
+  url: process.env.NEXT_PUBLIC_HOST,
+};
+
 import { schedule, bands, spots, events } from "./data";
 
 export default async function Home() {
