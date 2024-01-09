0b3ec5174f6d774bfb32545c73313c4e792e3282
Ballademager <118718923+Ballademager@users.noreply.github.com>
2024-01-09 14:50:54 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -369,7 +369,7 @@ function Header() {
         </g>
       </svg>
       <Marquee autoFill={true} pauseOnHover={true}>
-        <h2 className="date">17.10.2024 - 24.10.2024</h2>
+        <h2 className="festival-date">17.10.2024 - 24.10.2024</h2>
       </Marquee>
       <div className="header_container">
         <h1 className="headline headerh1">FOOFEST</h1>
