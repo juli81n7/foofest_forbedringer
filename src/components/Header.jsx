83367599794dbf3b98c15d8a7a246199a25ca396
Valentin <Chris_valentin@hotmail.com>
2024-01-11 13:11:40 +0100

    maybe?

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -339,13 +339,12 @@ function Header() {
         </g>
       </svg>
       <Marquee autoFill={true} pauseOnHover={true}>
+        <h2 className="festival-date">17.10.2024 - 24.10.2024</h2>
         {eventStatus.map((event) => (
-          <>
+          <div className="cancelled-box" key={event.act.act}>
+            <h2 className="cancelled-event">{event.act.act} CANCELLED</h2>
             <h2 className="festival-date">17.10.2024 - 24.10.2024</h2>
-            <h2 className="cancelled-event" key={event.act.act}>
-              {event.act.act} CANCELLED
-            </h2>
-          </>
+          </div>
         ))}
       </Marquee>
       <div className="header_container">
